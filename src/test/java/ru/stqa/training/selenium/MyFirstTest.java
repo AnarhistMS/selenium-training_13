208120f8bf1e5cf7d57162518575745df1785057
Aleksei Kokoulin <kokoulin21@gmail.com>
2020-07-29 15:39:17 +0300

    3 remarks fixed

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -29,26 +29,31 @@ public class MyFirstTest {
 
     @Test
     public void myFirstTest() {
-        for (int i = 0; i < 3; i++) {
-            driver.get("http://localhost/litecart/");
-            wait.until(titleIs("Online Store | My Store"));
+        driver.get("http://localhost/litecart/");
+        wait.until(titleIs("Online Store | My Store"));
+        int count = 0;
+        while (count < 3) {
+            int countBefore = Integer.parseInt(driver.findElement(By.xpath("//span[@class='quantity']")).getText());
             driver.findElement(By.xpath("//li[contains(@class,'product')]")).click();
             wait.until(titleContains("Rubber Ducks"));
-            WebElement sticker = driver.findElement(By.xpath("//div[contains(@class, 'sticker')]"));
-            if (sticker.getAttribute("title").equals("On Sale")) {
+            if (driver.findElements(By.xpath("//select[@name='options[Size]']")).size() != 0) {
                 Select size = new Select(driver.findElement(By.xpath("//select")));
                 size.selectByValue("Large");
                 System.out.println("size");
             }
             driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
-            wait.until(presenceOfElementLocated(By.xpath("//div[@id='cart']//img[contains(@src, 'cart_filled.png')]")));
+            wait.until(textToBe(By.xpath("//span[@class='quantity']"), String.valueOf(countBefore+1)));
+            driver.get("http://localhost/litecart/");
+            wait.until(titleIs("Online Store | My Store"));
+            count++;
         }
         driver.get("http://localhost/litecart/");
         driver.findElement(By.xpath("//div[@id='cart']//a[@class='link']")).click();
-        List<WebElement> duckAmount = driver.findElements(By.xpath("//button[text()='Remove']"));
-        for (int i = 0; i < duckAmount.size(); i++) {
-            duckAmount.get(i).click();
-            wait.until(stalenessOf(duckAmount.get(i)));
+        int duckCount = driver.findElements(By.xpath("//button[text()='Remove']")).size();
+        while (duckCount > 0) {
+            driver.findElement(By.xpath("//button[text()='Remove']")).click();
+            wait.until(stalenessOf(driver.findElement(By.xpath("//table[contains(@class,'dataTable')]"))));
+            duckCount--;
         }
     }
 
